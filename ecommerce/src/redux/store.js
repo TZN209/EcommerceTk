8d85266d35a4d2e6c2dd41ae37a0289e8976119b
TZN209 <vuhieucntt@gmail.com>
2023-02-07 12:54:41 +0700

    first commit

---
+++ ecommerce/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import productReducer from './slides/productSlide';
+import userReducer from './slides/userSlide';
+
+export const store = configureStore({
+    reducer: {
+        product: productReducer,
+        user: userReducer,
+    },
+});
